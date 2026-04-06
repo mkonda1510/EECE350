cbe3d290816db585fc5efd223e02f110478b2fdb
Mohamad Kondakji <mhk61@mail.aub.edu>
2026-04-06 13:14:41 +0300

    Created protocol.py

---
+++ 350 Proj/protocol.py
@@ -0,0 +1,15 @@
+#Protocols
+# Message types
+
+JOIN = "join"
+PLAYER_LIST = "player_list"
+SELECT_OPPONENT = "select_oppponent "
+MOVE = "move"
+GAME_STATE = "game_state"
+GAME_OVER = "game_over"
+
+# Directions
+UP = "UP"
+DOWN = "DOWN"
+LEFT = "LEFT"
+RIGHT = "RIGHT"
